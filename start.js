0073840d75a6a2998bc8f70d24a4541d0802b00e
Neio.zhou <neio.zhou@gmail.com>
2017-01-13 09:27:02 +0000

    port

--- start.js
+++ start.js
@@ -3,7 +3,7 @@
     console.info('bootstrap app');
     var pac     = require('./pac.js');
     var ip = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
-    var port = process.env.OPENSHIFT_NODEJS_PORT || 8888;
+    var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8888;
     var pacApp = new pac.PacApp();
     pacApp.start(ip, port);
 
